8474728b0ec1820ffd7affe87524b441719f4ffe
ych0218 <gtra464@gmail.com>
2026-03-31 13:12:25 +0900

    20260331

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,7 +1,21 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+import config
+
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(config)
+
+    # ORM
+    db.init_app(app)
+    migrate.init_app(app, db)
+    from . import models
 
     # 블루프린트 등록
     from .views import main_views
