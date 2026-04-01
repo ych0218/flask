70032d705ef96461ad711fced0169d48a8840ea5
ych0218 <gtra464@gmail.com>
2026-04-01 17:39:10 +0900

    20260401

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 import config
 
-
 db = SQLAlchemy()
 migrate = Migrate()
 
@@ -18,7 +17,9 @@ def create_app():
     from . import models
 
     # 블루프린트 등록
-    from .views import main_views
+    from .views import main_views, question_views, answer_views
     app.register_blueprint(main_views.bp)
+    app.register_blueprint(question_views.bp)
+    app.register_blueprint(answer_views.bp)
 
     return app
--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,19 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, redirect, url_for
+
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/')
 def index():
-    return 'Pybo index!'
+    return redirect(url_for('question._list'))
 
-@bp.route('/hello')
-def hello_world():
-    return 'Hello Pybo!'
 
-@bp.route('/login')
-def login():
-    return '로그인이 필요합니다.'
-
-@bp.route('/logout')
-def logout():
-    return '로그아웃'
